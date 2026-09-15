b65f1b69216a70fbbb82f7333ca835baf90ff096
DmitryBelkevich <harleydav@mail.ru>
2026-09-16 01:00:02 +0300

    Update ListView.js

--- app/views/ListView.js
+++ app/views/ListView.js
@@ -5,7 +5,7 @@ export default class ListView {
     this.cssLoader = new CssLoader();
     
     // reset css
-    this.cssLoader.load("./api/views/css/reset.css");
+    this.cssLoader.load("./app/views/css/reset.css");
     
     this.body = document.body;
   }
