a76c3f9d092aa615ac0f849ef04e13df131c29a6
DmitryBelkevich <harleydav@mail.ru>
2026-09-17 08:15:44 +0300

    Update SongService.js

--- app/services/SongService.js
+++ app/services/SongService.js
@@ -1,8 +1,8 @@
+import config from '../config/config.js';
+
 import SongDao from '../dao/SongDao.js';
 
 export default class SongService {
-  #storage = "./storage/Text & Chords/";
-  
   constructor() {
     this.songDao = new SongDao();
   }
@@ -16,7 +16,7 @@ export default class SongService {
   async getById(id) {
     const song = await this.songDao.getById(id);
 
-    song.text = this.#storage + song.text;
+    song.text = config.storage + song.text;
 
     song.instruments.forEach((instrument, index) => {
       if (instrument.title == "Guitar")
