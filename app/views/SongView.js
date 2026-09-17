2a4f765b7699a0c32b385794e5f69090413953ef
DmitryBelkevich <harleydav@mail.ru>
2026-09-17 22:25:36 +0300

    Update SongView.js

--- app/views/SongView.js
+++ app/views/SongView.js
@@ -51,9 +51,9 @@ export default class SongView {
     this.display.id = "display";
 
     // key-signature
-    this.title1_e = document.createElement("div");
-    this.title1_e.classList.add("title");
-    this.title1_e.textContent = "key: ";
+    const title1_e = document.createElement("div");
+    title1_e.classList.add("title");
+    title1_e.textContent = "key:";
     
     this.key_e = document.createElement("div");
     this.key_e.id = "key";
@@ -63,7 +63,10 @@ export default class SongView {
     this.select_e.id = "instruments";
 
     // fill display
-    this.display.append(this.title1_e, this.key_e, this.select_e);
+    this.display.append(
+      title1_e, this.key_e,
+      this.select_e,
+    );
     
     // Settings
     this.cssLoader.load("./app/views/css/song/settings.css");
