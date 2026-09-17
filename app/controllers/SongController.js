f3b15d805bc269d5d7351d31e2958295db38e9be
DmitryBelkevich <harleydav@mail.ru>
2026-09-18 00:11:25 +0300

    Update SongController.js

--- app/controllers/SongController.js
+++ app/controllers/SongController.js
@@ -85,6 +85,9 @@ export default class SongController {
 
   openText = () => {
     console.log("open Text tab");
+
+    const text = await this.htmlLoader.load(this.song.text_bass);
+    this.view.setText(text);
   }
 
   openScore = () => {
