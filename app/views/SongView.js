e4492dfdde3f4ea26fc71c77d7383b492e727692
DmitryBelkevich <harleydav@mail.ru>
2026-09-18 23:07:26 +0300

    Update SongView.js

--- app/views/SongView.js
+++ app/views/SongView.js
@@ -58,11 +58,11 @@ export default class SongView {
     // transposer
     this.transpose_down = document.createElement("button");
     this.transpose_down.id = "transpose_down";
-    this.transpose_down.textContent = "🔽 transpose";
+    this.transpose_down.textContent = "🔽";
 
     this.transpose_up = document.createElement("button");
     this.transpose_up.id = "transpose_up";
-    this.transpose_up.textContent = "🔼 transpose";
+    this.transpose_up.textContent = "🔼";
 
     // fill display
     this.display.append(this.key_e, this.transpose_down, this.transpose_up);
