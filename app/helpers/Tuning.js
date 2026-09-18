edb50b079f97be105a2b042b2b61dc7e49ccf926
DmitryBelkevich <harleydav@mail.ru>
2026-09-18 03:18:00 +0300

    Update Tuning.js

--- app/helpers/Tuning.js
+++ app/helpers/Tuning.js
@@ -21,76 +21,76 @@ export default class Tuning {
   }
 
   static async droppedTo(tuning) {
-    const isEqual = await ((a, b) => a.length === b.length && a.every((val, i) => val === b[i]));
+    const isEqual = (a, b) => a.length === b.length && a.every((val, i) => val === b[i]);
     
     // *** Guitars ***
     
     // standards
-    if (isEqual(tuning, ["E", "A", "D", "G", "B", "E"]))//0
+    if (await isEqual(tuning, ["E", "A", "D", "G", "B", "E"]))//0
       return 0;
 
-    if (isEqual(tuning, ["D#", "G#", "C#", "F#", "A#", "D#"]))//-1
+    if (await isEqual(tuning, ["D#", "G#", "C#", "F#", "A#", "D#"]))//-1
       return -1;
 
-    if (isEqual(tuning, ["D", "G", "C", "F", "A", "D"]))//-2
+    if (await isEqual(tuning, ["D", "G", "C", "F", "A", "D"]))//-2
       return -2;
 
-    if (isEqual(tuning, ["C#", "F#", "B", "E", "G#", "C#"]))//-3
+    if (await isEqual(tuning, ["C#", "F#", "B", "E", "G#", "C#"]))//-3
       return -3;
     
     // drops
-    if (isEqual(tuning, ["D", "A", "D", "G", "B", "E"]))//0
+    if (await isEqual(tuning, ["D", "A", "D", "G", "B", "E"]))//0
       return 0;
 
-    if (isEqual(tuning, ["C#", "G#", "C#", "F#", "A#", "D#"]))//-1
+    if (await isEqual(tuning, ["C#", "G#", "C#", "F#", "A#", "D#"]))//-1
       return -1;
 
-    if (isEqual(tuning, ["C", "G", "C", "F", "A", "D"]))//-2
+    if (await isEqual(tuning, ["C", "G", "C", "F", "A", "D"]))//-2
       return -2;
 
-    if (isEqual(tuning, ["B", "F#", "B", "E", "G#", "C#"]))//-3
+    if (await isEqual(tuning, ["B", "F#", "B", "E", "G#", "C#"]))//-3
       return -3;
 
     // *** Bass Guitars ***
 
     // standards
-    if (isEqual(tuning, ["E", "A", "D", "G"]))//0
+    if (await isEqual(tuning, ["E", "A", "D", "G"]))//0
       return 0;
 
-    if (isEqual(tuning, ["D#", "G#", "C#", "F#"]))//-1
+    if (await isEqual(tuning, ["D#", "G#", "C#", "F#"]))//-1
       return -1;
 
-    if (isEqual(tuning, ["D", "G", "C", "F"]))//-2
+    if (await isEqual(tuning, ["D", "G", "C", "F"]))//-2
       return -2;
 
-    if (isEqual(tuning, ["C#", "F#", "B", "E"]))//-3
+    if (await isEqual(tuning, ["C#", "F#", "B", "E"]))//-3
       return -3;
 
     // drops
-    if (isEqual(tuning, ["D", "A", "D", "G"]))//0
+    if (await isEqual(tuning, ["D", "A", "D", "G"]))//0
       return 0;
 
-    if (isEqual(tuning, ["C#", "G#", "C#", "F#"]))//-1
+    if (await isEqual(tuning, ["C#", "G#", "C#", "F#"]))//-1
       return -1;
 
-    if (isEqual(tuning, ["C", "G", "C", "F"]))//-2
+    if (await isEqual(tuning, ["C", "G", "C", "F"]))//-2
       return -2;
 
-    if (isEqual(tuning, ["B", "F#", "B", "E"]))//-3
+    if (await isEqual(tuning, ["B", "F#", "B", "E"]))//-3
       return -3;
 
     // *** 5-strings Bass Guitars ***
 
-    if (isEqual(tuning, ["B", "E", "A", "D", "G"]))//0
+    if (await isEqual(tuning, ["B", "E", "A", "D", "G"]))//0
       return 0;
 
-    if (isEqual(tuning, ["D#", "D#", "G#", "C#", "F#", "A#"]))//-1
+    if (await isEqual(tuning, ["D#", "D#", "G#", "C#", "F#", "A#"]))//-1
       return -1;
 
-    if (isEqual(tuning, ["A", "D", "G", "C", "F"]))//-2
+    if (await isEqual(tuning, ["A", "D", "G", "C", "F"]))//-2
       return -2;
 
-    if (isEqual(tuning, ["G#", "C#", "F#", "B", "E"]))//-3
+    if (await isEqual(tuning, ["G#", "C#", "F#", "B", "E"]))//-3
       return -3;
   }
 
