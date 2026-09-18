3f5bf7e41b9ed11b27d169252c609fb34afa0b0f
DmitryBelkevich <harleydav@mail.ru>
2026-09-18 03:49:08 +0300

    Update Tuning.js

--- app/helpers/Tuning.js
+++ app/helpers/Tuning.js
@@ -21,9 +21,7 @@ export default class Tuning {
   }
 
   static droppedTo(tuning) {
-    const isEqual = (a, b) => {
-      console.log(a);
-    };
+    const isEqual = (a, b) => a.length === b.length && a.every((val, i) => val === b[i]);
 
     isEqual(tuning, ["E", "A", "D", "G", "B", "E"]);
     
@@ -32,72 +30,72 @@ export default class Tuning {
     // *** Guitars ***
     
     // standards
-    // if (isEqual(tuning, ["E", "A", "D", "G", "B", "E"]))//0
-    //   return 0;
+    if (isEqual(tuning, ["E", "A", "D", "G", "B", "E"]))//0
+      return 0;
 
-    // if (isEqual(tuning, ["D#", "G#", "C#", "F#", "A#", "D#"]))//-1
-    //   return -1;
+    if (isEqual(tuning, ["D#", "G#", "C#", "F#", "A#", "D#"]))//-1
+      return -1;
 
-    // if (isEqual(tuning, ["D", "G", "C", "F", "A", "D"]))//-2
-    //   return -2;
+    if (isEqual(tuning, ["D", "G", "C", "F", "A", "D"]))//-2
+      return -2;
 
-    // if (isEqual(tuning, ["C#", "F#", "B", "E", "G#", "C#"]))//-3
-    //   return -3;
+    if (isEqual(tuning, ["C#", "F#", "B", "E", "G#", "C#"]))//-3
+      return -3;
     
-    // // drops
-    // if (isEqual(tuning, ["D", "A", "D", "G", "B", "E"]))//0
-    //   return 0;
+    // drops
+    if (isEqual(tuning, ["D", "A", "D", "G", "B", "E"]))//0
+      return 0;
 
-    // if (isEqual(tuning, ["C#", "G#", "C#", "F#", "A#", "D#"]))//-1
-    //   return -1;
+    if (isEqual(tuning, ["C#", "G#", "C#", "F#", "A#", "D#"]))//-1
+      return -1;
 
-    // if (isEqual(tuning, ["C", "G", "C", "F", "A", "D"]))//-2
-    //   return -2;
+    if (isEqual(tuning, ["C", "G", "C", "F", "A", "D"]))//-2
+      return -2;
 
-    // if (isEqual(tuning, ["B", "F#", "B", "E", "G#", "C#"]))//-3
-    //   return -3;
+    if (isEqual(tuning, ["B", "F#", "B", "E", "G#", "C#"]))//-3
+      return -3;
 
-    // // *** Bass Guitars ***
+    // *** Bass Guitars ***
 
-    // // standards
-    // if (isEqual(tuning, ["E", "A", "D", "G"]))//0
-    //   return 0;
+    // standards
+    if (isEqual(tuning, ["E", "A", "D", "G"]))//0
+      return 0;
 
-    // if (isEqual(tuning, ["D#", "G#", "C#", "F#"]))//-1
-    //   return -1;
+    if (isEqual(tuning, ["D#", "G#", "C#", "F#"]))//-1
+      return -1;
 
-    // if (isEqual(tuning, ["D", "G", "C", "F"]))//-2
-    //   return -2;
+    if (isEqual(tuning, ["D", "G", "C", "F"]))//-2
+      return -2;
 
-    // if (isEqual(tuning, ["C#", "F#", "B", "E"]))//-3
-    //   return -3;
+    if (isEqual(tuning, ["C#", "F#", "B", "E"]))//-3
+      return -3;
 
-    // // drops
-    // if (isEqual(tuning, ["D", "A", "D", "G"]))//0
-    //   return 0;
+    // drops
+    if (isEqual(tuning, ["D", "A", "D", "G"]))//0
+      return 0;
 
-    // if (isEqual(tuning, ["C#", "G#", "C#", "F#"]))//-1
-    //   return -1;
+    if (isEqual(tuning, ["C#", "G#", "C#", "F#"]))//-1
+      return -1;
 
-    // if (isEqual(tuning, ["C", "G", "C", "F"]))//-2
-    //   return -2;
+    if (isEqual(tuning, ["C", "G", "C", "F"]))//-2
+      return -2;
 
-    // if (isEqual(tuning, ["B", "F#", "B", "E"]))//-3
-    //   return -3;
+    if (isEqual(tuning, ["B", "F#", "B", "E"]))//-3
+      return -3;
 
-    // // *** 5-strings Bass Guitars ***
+    // *** 5-strings Bass Guitars ***
 
-    // if (isEqual(tuning, ["B", "E", "A", "D", "G"]))//0
-    //   return 0;
+    if (isEqual(tuning, ["B", "E", "A", "D", "G"]))//0
+      return 0;
 
-    // if (isEqual(tuning, ["D#", "D#", "G#", "C#", "F#", "A#"]))//-1
-    //   return -1;
+    if (isEqual(tuning, ["D#", "D#", "G#", "C#", "F#", "A#"]))//-1
+      return -1;
 
-    // if (isEqual(tuning, ["A", "D", "G", "C", "F"]))//-2
-    //   return -2;
+    if (isEqual(tuning, ["A", "D", "G", "C", "F"]))//-2
+      return -2;
 
-    // if (isEqual(tuning, ["G#", "C#", "F#", "B", "E"]))//-3
-    //   return -3;
+    if (isEqual(tuning, ["G#", "C#", "F#", "B", "E"]))//-3
+      return -3;
   }
 
   static getTitle(tuning) {
