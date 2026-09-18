295276b545be4226ea01f0d90485451358fc0e7c
DmitryBelkevich <harleydav@mail.ru>
2026-09-19 02:05:32 +0300

    Update Guitar.js

--- app/models/Guitar.js
+++ app/models/Guitar.js
@@ -1,12 +1,22 @@
 import Instrument from "./Instrument.js";
 
 export default class Guitar extends Instrument {
-  #tuning = [];
+  #tuning;
   #capo = 0;
   
   constructor() {
     super();
     this._title = "Guitar";
+
+    this.#tuning = ["E", "A", "D", "G", "B", "E"];
+  }
+
+  set tuning(tuning) {
+    this.#tuning = tuning;
+  }
+
+  get tuning() {
+    return this.#tuning;
   }
 
   set capo(capo) {
