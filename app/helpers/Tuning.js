63c02862f587464a787e42a3cc5dcbccda04d798
DmitryBelkevich <harleydav@mail.ru>
2026-09-18 03:10:40 +0300

    Update Tuning.js

--- app/helpers/Tuning.js
+++ app/helpers/Tuning.js
@@ -21,9 +21,7 @@ export default class Tuning {
   }
 
   static async droppedTo(tuning) {
-    const isEqual = await (a, b) => {
-      // a.length === b.length && a.every((val, i) => val === b[i])
-    };
+    const isEqual = await ((a, b) => {});
     
     // *** Guitars ***
     
