388a153c0e3073ae2edbf70710b0579a410f62ea
DmitryBelkevich <harleydav@mail.ru>
2026-09-18 06:47:33 +0300

    Update SongService.js

--- app/services/SongService.js
+++ app/services/SongService.js
@@ -26,23 +26,23 @@ export default class SongService {
         if (!instrument.transposition)
           instrument.transposition = 0;
       
-      if (instrument.title == "Guitar" || instrument.title == "E.Guitar")
+      else if (instrument.title == "Guitar" || instrument.title == "E.Guitar")
         if (!instrument.tuning)
           instrument.tuning = ["E", "A", "D", "G", "B", "E"];
 
-      if (instrument.title == "Bass Guitar")
+      else if (instrument.title == "Bass Guitar")
         if (!instrument.tuning)
           instrument.tuning = ["E", "A", "D", "G"];
 
-      if (instrument.title == "5-string Bass Guitar")
+      else if (instrument.title == "5-string Bass Guitar")
         if (!instrument.tuning)
           instrument.tuning = ["B", "E", "A", "D", "G"];
 
-      if (instrument.title != "Keyboards")
+      else if (instrument.title != "Keyboards")
         if (!instrument.capo)
           instrument.capo = 0;
 
-      if (instrument.title == "Keyboards")
+      else if (instrument.title == "Keyboards")
         if (!instrument.transposition)
           instrument.transposition = 0;
     });
