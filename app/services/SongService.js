205bdc132b4b6ad70bfe75a5c620a8c54cdaf3b9
DmitryBelkevich <harleydav@mail.ru>
2026-09-18 03:46:23 +0300

    Update SongService.js

--- app/services/SongService.js
+++ app/services/SongService.js
@@ -19,22 +19,7 @@ export default class SongService {
     const song = await this.songDao.getById(id);
 
     song.text = config.storage + song.text;
-
-    song.instruments.forEach((instrument) => {
-      instrument.transposition = Tuning.droppedTo(instrument.tuning);
-    });
-
-    song.instruments.forEach((instrument) => {
-      const index = song.text.length - ".html".length;
-      
-      if (instrument.capo > 0)
-        instrument.chords = song.text.slice(0, index) + " (" + instrument.capo + ")" + song.text.slice(index);
-      else if (instrument.transposition < 0)
-        instrument.chords = song.text.slice(0, index) + " (" + instrument.transposition + ")" + song.text.slice(index);
-      else
-        instrument.chords = song.text;
-    });
-
+    
     song.instruments.forEach((instrument, index) => {
       if (instrument.title == "Guitar")
         if (!instrument.tuning)
@@ -55,6 +40,21 @@ export default class SongService {
       if (instrument.title == "Keyboards")
         if (!instrument.transposition)
           instrument.transposition = 0;
+    });
+
+    song.instruments.forEach((instrument) => {
+      instrument.transposition = Tuning.droppedTo(instrument.tuning);
+    });
+
+    song.instruments.forEach((instrument) => {
+      const index = song.text.length - ".html".length;
+      
+      if (instrument.capo > 0)
+        instrument.chords = song.text.slice(0, index) + " (" + instrument.capo + ")" + song.text.slice(index);
+      else if (instrument.transposition < 0)
+        instrument.chords = song.text.slice(0, index) + " (" + instrument.transposition + ")" + song.text.slice(index);
+      else
+        instrument.chords = song.text;
     });
     
     return song;
