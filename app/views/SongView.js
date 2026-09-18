79ca2b5433b3258aca3695debb95d8ab63489f19
DmitryBelkevich <harleydav@mail.ru>
2026-09-18 20:24:46 +0300

    Update SongView.js

--- app/views/SongView.js
+++ app/views/SongView.js
@@ -186,7 +186,7 @@ export default class SongView {
   
   bindTextTab(handler) {
     this.tab_text.addEventListener("click", () => {
-      handler();this.selectOption(1)
+      handler();
     });
   }
 
