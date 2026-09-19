76291e13ec7b2517408171c468681f911e03d478
DmitryBelkevich <harleydav@mail.ru>
2026-09-19 15:59:45 +0300

    Update SongDao.js

--- app/dao/SongDao.js
+++ app/dao/SongDao.js
@@ -21,7 +21,7 @@ export default class SongDao {
     if (!result)
       return null;
 
-    const song = Object.setPrototypeOf(result, Song.prototype);
+    const song = Object.setPrototypeOf(data, Song.prototype);
 
     // song.id = result.id;
     // song.band = result.band;
