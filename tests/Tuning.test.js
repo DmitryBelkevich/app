487e2ebec3454b123f6778c8ff4b79a20060649c
DmitryBelkevich <harleydav@mail.ru>
2026-09-19 04:17:31 +0300

    Update Tuning.test.js

--- tests/Tuning.test.js
+++ tests/Tuning.test.js
@@ -3,7 +3,8 @@ import Tuning from "../app/models/Tuning.js";
 export default class TuningTest {
   test() {
     const tuning = new Tuning();
-    
+
+    console.log("must be: " + false);
     console.log(tuning.isStandard() + " | " + false);
   }
 }
