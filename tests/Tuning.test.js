680aa826dba09343f83f711f6989e5467a39136e
DmitryBelkevich <harleydav@mail.ru>
2026-09-19 04:19:42 +0300

    Update Tuning.test.js

--- tests/Tuning.test.js
+++ tests/Tuning.test.js
@@ -4,6 +4,7 @@ export default class TuningTest {
   test() {
     const tuning = new Tuning();
 
+    console.log("Tuning.isStandard()");
     console.log("must be: " + false);
     console.log(tuning.isStandard() + " | " + false);
   }
