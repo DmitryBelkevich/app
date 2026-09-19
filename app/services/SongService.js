109f149e7a455bc0281d8e907fdeed5b8947c1b7
DmitryBelkevich <harleydav@mail.ru>
2026-09-19 16:19:22 +0300

    Update SongService.js

--- app/services/SongService.js
+++ app/services/SongService.js
@@ -36,7 +36,7 @@ export default class SongService {
         const keyboards = new Keyboards();
       } else if (instrument.title == "Guitar" || instrument.title == "E.Guitar" || instrument.title == "Bass Guitar" || instrument.title == "5-string Bass Guitar") {
         if (instrument.title == "Guitar" || instrument.title == "E.Guitar") {
-          // console.log(instrument);
+          console.log(instrument);
           // const guitar = Object.setPrototypeOf(instrument, Guitar.prototype);
           // console.log(guitar);
         } else if (instrument.title == "Bass Guitar") {
