88a61310bf5bd78ce31a4e2b1a660edfe6f08ff2
DmitryBelkevich <harleydav@mail.ru>
2026-09-20 08:56:38 +0300

    Update FiveStringBassGuitar.js

--- app/models/FiveStringBassGuitar.js
+++ app/models/FiveStringBassGuitar.js
@@ -1,9 +1,10 @@
 import BassGuitar from "./BassGuitar.js";
+import Tuning from "./Tuning.js";
 
 export default class FiveStringBassGuitar extends BassGuitar {
   constructor() {
     super();
     this.title = "5-string Bass Guitar";
-    this.tuning = ["B", "E", "A", "D", "G"];
+    this.tuning = new Tuning("B", "E", "A", "D", "G");
   }
 }
