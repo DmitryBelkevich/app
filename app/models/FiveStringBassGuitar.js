8f85abd902ef282535a87542b2e8d1b7b58280f9
DmitryBelkevich <harleydav@mail.ru>
2026-09-20 08:46:53 +0300

    Update and rename _5StringBassGuitar.js to FiveStringBassGuitar.js

--- app/models/FiveStringBassGuitar.js
+++ app/models/FiveStringBassGuitar.js
@@ -1,6 +1,6 @@
 import BassGuitar from "./BassGuitar.js";
 
-export default class _5StringBassGuitar extends BassGuitar {
+export default class FiveStringBassGuitar extends BassGuitar {
   constructor() {
     super();
     this.title = "5-string Bass Guitar";
