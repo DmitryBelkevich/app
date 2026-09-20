93dfb71ccae9399eb7b82231419789e04a797da9
DmitryBelkevich <harleydav@mail.ru>
2026-09-20 16:26:56 +0300

    Update Cookie.js

--- app/memento/Cookie.js
+++ app/memento/Cookie.js
@@ -10,9 +10,8 @@ export default class Cookie {
     
     const object = objects.find(obj => obj.split("=")[0] == "current") || null;
     
-    if (!object) {
-      console.log(object);
-      // return object.split("=")[1];
+    if (object != null) {
+      return object.split("=")[1];
     }
     
     return null;
