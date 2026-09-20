c992b95bd8a43d2484ec3418985918d3cee74477
DmitryBelkevich <harleydav@mail.ru>
2026-09-20 07:46:12 +0300

    Update SongService.js

--- app/services/SongService.js
+++ app/services/SongService.js
@@ -55,7 +55,8 @@ export default class SongService {
           guitar.title = instrument.title;
           
           if (instrument.tuning) {
-            guitar.tuning = new Tuning("n", "e", "w");
+            guitar.tuning = new Tuning("n");
+            guitar.tuning.push("e", "w");
           }
           
           guitar.capo = instrument.capo;
