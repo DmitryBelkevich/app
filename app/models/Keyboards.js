87e72707ec6139fe937771844f919a5bd72b8eec
DmitryBelkevich <harleydav@mail.ru>
2026-09-20 09:32:30 +0300

    Update Keyboards.js

--- app/models/Keyboards.js
+++ app/models/Keyboards.js
@@ -1,19 +1,8 @@
 import Instrument from "./Instrument.js";
 
 export default class Keyboards extends Instrument {
-  transposition;
-  
   constructor() {
     super();
     this.title = "Keyboards";
-    this.transposition = 0;
-  }
-
-  get transposition() {
-    return this.transposition;
-  }
-
-  set transposition(transposition) {
-    this.transposition = transposition;
   }
 }
