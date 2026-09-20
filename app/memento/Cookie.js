a63e0033cf8b80f2b9c49bab8f0649ad4dd9b53a
DmitryBelkevich <harleydav@mail.ru>
2026-09-20 15:10:44 +0300

    Update Cookie.js

--- app/memento/Cookie.js
+++ app/memento/Cookie.js
@@ -1,17 +1,11 @@
 export default class Cookie {
-  // #cookies = [];
   #current;
   
-  constructor() {
-    // this.#cookies = document.cookie.split("; ");
-  }
-
   get current() {
     return this.#current;
   }
   
   set current(current) {
-    // document.cookie = "index=" + index;
     this.#current = current;
   }
 }
