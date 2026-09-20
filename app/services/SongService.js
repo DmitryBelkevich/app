ffe7e7e04ef37a986b18c9ad1e6ed3dfc5aa724a
DmitryBelkevich <harleydav@mail.ru>
2026-09-20 08:47:53 +0300

    Update SongService.js

--- app/services/SongService.js
+++ app/services/SongService.js
@@ -8,7 +8,7 @@ import Instrument from '../models/Instrument.js';
 import Keyboards from '../models/Keyboards.js';
 import Guitar from '../models/Guitar.js';
 import BassGuitar from '../models/BassGuitar.js';
-import FiveStringBassGuitar from '../models/_5StringBassGuitar.js';
+import FiveStringBassGuitar from '../models/FiveStringBassGuitar.js';
 
 export default class SongService {
   constructor() {
