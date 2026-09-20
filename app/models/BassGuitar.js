6bcdfd20d0a8d392a8a37ea104ddf49788e30fb3
DmitryBelkevich <harleydav@mail.ru>
2026-09-20 11:32:36 +0300

    Update BassGuitar.js

--- app/models/BassGuitar.js
+++ app/models/BassGuitar.js
@@ -5,6 +5,7 @@ export default class BassGuitar extends Guitar {
   constructor() {
     super();
     this.title = "Bass Guitar";
+    this.color = "yellow";
     this.tuning = new Tuning("E", "A", "D", "G");
   }
 }
