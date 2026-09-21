ef156cf68b344142c7a78d3514bd28085279dc9c
DmitryBelkevich <harleydav@mail.ru>
2026-09-21 14:16:32 +0300

    Update CookieSaver.js

--- app/memento/CookieSaver.js
+++ app/memento/CookieSaver.js
@@ -14,7 +14,7 @@ export default class CookieSaver {
 
     const value = cookies_obj[name];
 
-    console.log(value);
+    console.log(cookies_obj);
 
     const result = {name: name, value: value};
     
