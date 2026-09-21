8c359f0f6da6d285f12a3687fd4059980bf1405a
DmitryBelkevich <harleydav@mail.ru>
2026-09-21 11:52:13 +0300

    Update SongController.js

--- app/controllers/SongController.js
+++ app/controllers/SongController.js
@@ -17,17 +17,21 @@ export default class SongController {
     this.#params = new URLSearchParams(window.location.search);
     const id = this.#params.get("id");
     
-    this.#cookieSaver = new CookieSaver();
-    const obj = this.#cookieSaver.getByName("current") || {name: "current", value: 0};
-    this.#cookieSaver.save(obj);
-    this.#current = obj.value;
-    
     // model
     this.songService = new SongService();
     this.song = await this.songService.getById(id);
 
-    console.log(this.song);
-    console.log("current: " + this.#current);
+    // cookie
+    this.#cookieSaver = new CookieSaver();
+    const default_obj = {name: "curent", value: 0};
+    
+    const obj = this.#cookieSaver.getByName("current") || default_obj;
+    this.#cookieSaver.save(obj);
+
+    if (this.song.instruments.length > 1)
+      this.#current = obj.value;
+    else
+      this.#current = default_obj.value;
 
     // view
     this.view = new SongView();
