72f9cf2aa97a4ba946d1a3cb14e79253c22ce905
DmitryBelkevich <harleydav@mail.ru>
2026-09-21 15:28:40 +0300

    Update SongController.js

--- app/controllers/SongController.js
+++ app/controllers/SongController.js
@@ -28,6 +28,7 @@ export default class SongController {
     const cookie_obj = this.#cookieSaver.getByName("current") || default_cookie_obj;
     this.#cookieSaver.save(cookie_obj);
 
+    console.log(this.#current);
     if (this.song.instruments.length > this.#current)
       this.#current = cookie_obj.value;
     else
