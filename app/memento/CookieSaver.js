f0399a0fc67b8dd401f1eacdc100105e5aee6ce6
DmitryBelkevich <harleydav@mail.ru>
2026-09-21 10:54:58 +0300

    Update CookieSaver.js

--- app/memento/CookieSaver.js
+++ app/memento/CookieSaver.js
@@ -1,10 +1,8 @@
 export default class CookieSaver {
-  #current = "current";
-  
-  get current() {
+  getByName(name) {
     const objects = document.cookie.split("; ");
     
-    const object = objects.find(obj => obj.split("=")[0] == this.#current) || null;
+    const object = objects.find(obj => obj.split("=")[0] == name) || null;
     
     if (!!object)
       return object.split("=")[1];
@@ -12,7 +10,7 @@ export default class CookieSaver {
     return null;
   }
   
-  set current(current) {
-    document.cookie = this.#current + "=" + current;
+  save(obj) {
+    document.cookie = obj.name + "=" + obj.value;
   }
 }
