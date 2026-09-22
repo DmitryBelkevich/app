87877d8f04e408784901fd113e6532f336461961
DmitryBelkevich <harleydav@mail.ru>
2026-09-23 00:18:59 +0300

    Update SongController.js

--- app/controllers/SongController.js
+++ app/controllers/SongController.js
@@ -61,7 +61,6 @@ export default class SongController {
 
     // state
     this.stateService = new StateService(this.song, this.view);
-    this.stateService.current = 1;
     this.stateService.load();
 
     // *** binding controller-view ***
