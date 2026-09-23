3d1445c50303250b1ffc46414e85313d18039d55
DmitryBelkevich <harleydav@mail.ru>
2026-09-23 06:50:17 +0300

    Update TransposeService.js

--- app/services/TransposeService.js
+++ app/services/TransposeService.js
@@ -1,16 +1,19 @@
 export default class TransposeService {
   #chords = [];
-  
-  constructor() {
+
+  constructor(song, view) {
+    this.song = song;
+    this.view = view;
+  }
+
+  getChords() {
     const lines = document.querySelectorAll('div.chords');
     
     lines.forEach(line => {
     const line_arr = line.querySelectorAll('div');
       this.#chords.push(...line_arr);
     });
-  }
-
-  print() {
+    
     console.log(this.#chords);
   }
 }
