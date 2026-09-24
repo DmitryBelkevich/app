9bccbadbf242dfb17e5edfd2dbea2c9aadd7a444
DmitryBelkevich <harleydav@mail.ru>
2026-09-24 15:19:20 +0300

    Update GenreDao.js

--- app/dao/GenreDao.js
+++ app/dao/GenreDao.js
@@ -14,7 +14,7 @@ export default class GenreDao {
     const set = new Set();
 
     // data.forEach(song => set.add(song.genre));
-    set.add(data.forEach(() => song.genre));
+    set.add(data.forEach((song) => song.genre));
 
     console.log(set);
     
