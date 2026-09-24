fef640697aeb0f3e3b88873a0575f7c00ae827c7
DmitryBelkevich <harleydav@mail.ru>
2026-09-25 00:09:43 +0300

    Update ListView.js

--- app/views/ListView.js
+++ app/views/ListView.js
@@ -34,7 +34,8 @@ export default class ListView {
     element.textContent = band + " - " + title;
 
     element.addEventListener('click', function(event) {
-      window.open("https://dmitrybelkevich.github.io/app/song?id=" + id);
+      // window.open("https://dmitrybelkevich.github.io/app/song?id=" + id);
+      window.location.href = "https://dmitrybelkevich.github.io/app/song?id=" + id;
     });
 
     this.body.append(element);
