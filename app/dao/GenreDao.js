88d3f0d17193b5abadca9311364d39e9aa467897
DmitryBelkevich <harleydav@mail.ru>
2026-09-24 14:43:22 +0300

    Update GenreDao.js

--- app/dao/GenreDao.js
+++ app/dao/GenreDao.js
@@ -1,7 +1,16 @@
 import config from '../config/config.js';
 
+import Song from '../models/Song.js';
+import JsonLoader from '../loaders/JsonLoader.js';
+
 export default class GenreDao {
+  constructor() {
+    this.jsonLoader = new JsonLoader();
+  }
+  
   async getAll() {
+    const data = await this.jsonLoader.load(config.database + "songs.json");
+    
     return [];
   }
 }
