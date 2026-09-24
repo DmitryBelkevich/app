9c0612d464e616b0fba0abc7a4f22a9ec0c3dea3
DmitryBelkevich <harleydav@mail.ru>
2026-09-24 21:36:01 +0300

    Update GenreDao.js

--- app/dao/GenreDao.js
+++ app/dao/GenreDao.js
@@ -1,6 +1,7 @@
 import config from '../config/config.js';
 
 import Genre from '../models/Genre.js';
+import Song from '../models/Song.js';
 import JsonLoader from '../loaders/JsonLoader.js';
 
 export default class GenreDao {
@@ -13,21 +14,28 @@ export default class GenreDao {
 
     console.log(data);
 
-    const set = new Set();
-    data.forEach(song => set.add(song.genre));
-
-    const genres = Array.from(set);
-    genres.forEach((title, index) => {
-      const genre = new Genre();
-
-      genre.id = index;
-      genre.title = title;
-
-      genres[index] = genre;
+    data.forEach((element) => {
+      const song = new Song();
+      console.log(song);
     });
 
-    console.log(genres);
+    // const genresSet = new Set();
+    // data.forEach(song => genresSet.add(song.genre));
+
+    // const genres = Array.from(genresSet);
+    // genres.forEach((title, index) => {
+    //   const genre = new Genre();
+
+    //   genre.id = index;
+    //   genre.title = title;
+
+    //   genres[index] = genre;
+
+      // add genre.song
+    // });
+
+    // console.log(genres);
     
-    return genres;
+    return [];//genres
   }
 }
