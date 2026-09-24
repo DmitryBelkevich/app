15a9649c2fa771392b10e6501a13caeced513449
DmitryBelkevich <harleydav@mail.ru>
2026-09-24 22:57:04 +0300

    Update ListController.js

--- app/controllers/ListController.js
+++ app/controllers/ListController.js
@@ -1,12 +1,12 @@
-import SongService from '../services/SongService.js';
+// import SongService from '../services/SongService.js';
 import GenreService from '../services/GenreService.js';
 import ListView from '../views/ListView.js';
 
 export default class ListController {
   async init() {
     // model
-    this.songService = new SongService();
-    this.songs = await this.songService.getAll();
+    // this.songService = new SongService();
+    // this.songs = await this.songService.getAll();
 
     this.genreService = new GenreService();
     this.genres = await this.genreService.getAll();
@@ -16,10 +16,10 @@ export default class ListController {
 
     this.genres.forEach((genre) => {
       this.view.addGenre(genre.title);
-    });
 
-    this.songs.forEach((song) => {
-      this.view.addSong(song.id, song.key, song.band, song.title);
+      genre.songs.forEach((song) => {
+        this.view.addSong(song.id, song.key, song.band, song.title);
+      });
     });
   }
 }
